3355d1beb5217d9d444ed0fe4d541c30e990b709
Spencer Bartholomew <38776747+konga400@users.noreply.github.com>
2022-08-04 12:09:32 -0600

    docs and gitignore

---
+++ README.md
@@ -0,0 +1,19 @@
+# Rocket Backend
+
+This repo shows what you can build with Rust and Rocket
+
+## Running
+
+We have downloaded `cargo-watch` that allows auto-reload of the Rocket server. To run the server with auto-realoading run the following command
+
+```Bash
+cargo watch -x run
+```
+
+## Build and Deploy the backend
+
+To compile to a production-ready executable, run the following command
+
+```Bash
+cargo build --release
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/hello/<name>")]
+fn hello(name: String) -> String {
+    format!("Hello, {}! You are the greatest programmer ever", name)
+}
+
+#[post("/hello", format = "json", data = "<name>")]
+fn hello_post(name: String) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index, hello, hello_post])
+        .mount("/this", routes![index, hello, hello_post])
 }
